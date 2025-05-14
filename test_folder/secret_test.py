fb6f3bed37c6d4ef2ca4ba8ce42d0e06653f360a
samhaf0809 <samhaf0809@highgateschool.org.uk>
2025-05-14 13:01:57 +0100

    secret

---
+++ test_folder/secret_test.py
@@ -0,0 +1,20 @@
+import secrets
+
+# Generate a random integer from 0 to 99
+random_number = secrets.randbelow(100)
+print(random_number)
+
+token1 = secrets.token_bytes()
+token2 = secrets.token_bytes(10)
+token3 = secrets.token_hex()
+token4 = secrets.token_hex(10)
+
+
+print(token1)
+print(token2)
+print(token3)
+print(token4)
+print(type(token1))
+print(type(token3))
+#print(int(token1,2))
+print(int(token3,16))
